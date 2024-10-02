d1ab2f5830fd61fd953bacb874b40ef7900d6ea5
22-48810-3@student.aiub.edu <htsadia2105@gmail.com>
2024-10-02 16:11:19 +0600

    ReturnBooks

---
+++ Try/ReturnBook.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace Try
+{
+    partial class ReturnBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReturnBook));
+            this.btnCancelRB = new System.Windows.Forms.Button();
+            this.btnReturnRB = new System.Windows.Forms.Button();
+            this.dtpBookReturnDateRB = new System.Windows.Forms.DateTimePicker();
+            this.txtBookIssueDateRB = new System.Windows.Forms.TextBox();
+            this.txtBookNameRB = new System.Windows.Forms.TextBox();
+            this.lblBookReturnDateRB = new System.Windows.Forms.Label();
+            this.lblBookIssueDateRB = new System.Windows.Forms.Label();
+            this.lblBookNameRB = new System.Windows.Forms.Label();
+            this.pnlInfoRB = new System.Windows.Forms.Panel();
+            this.btnExitRB = new System.Windows.Forms.Button();
+            this.btnRefreshRB = new System.Windows.Forms.Button();
+            this.pbxStuIs = new System.Windows.Forms.PictureBox();
+            this.btnSearchIsB = new System.Windows.Forms.Button();
+            this.txtEnrollIsB = new System.Windows.Forms.TextBox();
+            this.dgvReturnBook = new System.Windows.Forms.DataGridView();
+            this.lblEnterIsB = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlInfoRB.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxStuIs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReturnBook)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancelRB
+            // 
+            this.btnCancelRB.BackColor = System.Drawing.Color.Peru;
+            this.btnCancelRB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancelRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelRB.Location = new System.Drawing.Point(616, 134);
+            this.btnCancelRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancelRB.Name = "btnCancelRB";
+            this.btnCancelRB.Size = new System.Drawing.Size(113, 44);
+            this.btnCancelRB.TabIndex = 21;
+            this.btnCancelRB.Text = "Cancel";
+            this.btnCancelRB.UseVisualStyleBackColor = false;
+            // 
+            // btnReturnRB
+            // 
+            this.btnReturnRB.BackColor = System.Drawing.Color.Peru;
+            this.btnReturnRB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReturnRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturnRB.Location = new System.Drawing.Point(616, 60);
+            this.btnReturnRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReturnRB.Name = "btnReturnRB";
+            this.btnReturnRB.Size = new System.Drawing.Size(113, 43);
+            this.btnReturnRB.TabIndex = 20;
+            this.btnReturnRB.Text = "Return";
+            this.btnReturnRB.UseVisualStyleBackColor = false;
+            // 
+            // dtpBookReturnDateRB
+            // 
+            this.dtpBookReturnDateRB.Location = new System.Drawing.Point(267, 142);
+            this.dtpBookReturnDateRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpBookReturnDateRB.Name = "dtpBookReturnDateRB";
+            this.dtpBookReturnDateRB.Size = new System.Drawing.Size(253, 22);
+            this.dtpBookReturnDateRB.TabIndex = 18;
+            // 
+            // txtBookIssueDateRB
+            // 
+            this.txtBookIssueDateRB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookIssueDateRB.Location = new System.Drawing.Point(267, 76);
+            this.txtBookIssueDateRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookIssueDateRB.Name = "txtBookIssueDateRB";
+            this.txtBookIssueDateRB.Size = new System.Drawing.Size(253, 26);
+            this.txtBookIssueDateRB.TabIndex = 17;
+            this.txtBookIssueDateRB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBookNameRB
+            // 
+            this.txtBookNameRB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookNameRB.Location = new System.Drawing.Point(267, 17);
+            this.txtBookNameRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookNameRB.Name = "txtBookNameRB";
+            this.txtBookNameRB.Size = new System.Drawing.Size(253, 26);
+            this.txtBookNameRB.TabIndex = 16;
+            this.txtBookNameRB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBookReturnDateRB
+            // 
+            this.lblBookReturnDateRB.AutoSize = true;
+            this.lblBookReturnDateRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookReturnDateRB.Location = new System.Drawing.Point(65, 142);
+            this.lblBookReturnDateRB.Name = "lblBookReturnDateRB";
+            this.lblBookReturnDateRB.Size = new System.Drawing.Size(157, 24);
+            this.lblBookReturnDateRB.TabIndex = 2;
+            this.lblBookReturnDateRB.Text = "Book Return Date";
+            // 
+            // lblBookIssueDateRB
+            // 
+            this.lblBookIssueDateRB.AutoSize = true;
+            this.lblBookIssueDateRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookIssueDateRB.Location = new System.Drawing.Point(65, 76);
+            this.lblBookIssueDateRB.Name = "lblBookIssueDateRB";
+            this.lblBookIssueDateRB.Size = new System.Drawing.Size(145, 24);
+            this.lblBookIssueDateRB.TabIndex = 1;
+            this.lblBookIssueDateRB.Text = "Book Issue Date";
+            // 
+            // lblBookNameRB
+            // 
+            this.lblBookNameRB.AutoSize = true;
+            this.lblBookNameRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookNameRB.Location = new System.Drawing.Point(65, 21);
+            this.lblBookNameRB.Name = "lblBookNameRB";
+            this.lblBookNameRB.Size = new System.Drawing.Size(109, 24);
+            this.lblBookNameRB.TabIndex = 0;
+            this.lblBookNameRB.Text = "Book Name";
+            // 
+            // pnlInfoRB
+            // 
+            this.pnlInfoRB.Controls.Add(this.btnCancelRB);
+            this.pnlInfoRB.Controls.Add(this.btnReturnRB);
+            this.pnlInfoRB.Controls.Add(this.dtpBookReturnDateRB);
+            this.pnlInfoRB.Controls.Add(this.txtBookIssueDateRB);
+            this.pnlInfoRB.Controls.Add(this.txtBookNameRB);
+            this.pnlInfoRB.Controls.Add(this.lblBookReturnDateRB);
+            this.pnlInfoRB.Controls.Add(this.lblBookIssueDateRB);
+            this.pnlInfoRB.Controls.Add(this.lblBookNameRB);
+            this.pnlInfoRB.Location = new System.Drawing.Point(327, 469);
+            this.pnlInfoRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlInfoRB.Name = "pnlInfoRB";
+            this.pnlInfoRB.Size = new System.Drawing.Size(907, 198);
+            this.pnlInfoRB.TabIndex = 7;
+            // 
+            // btnExitRB
+            // 
+            this.btnExitRB.BackColor = System.Drawing.Color.Peru;
+            this.btnExitRB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExitRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExitRB.Location = new System.Drawing.Point(181, 378);
+            this.btnExitRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExitRB.Name = "btnExitRB";
+            this.btnExitRB.Size = new System.Drawing.Size(113, 37);
+            this.btnExitRB.TabIndex = 20;
+            this.btnExitRB.Text = "Exit";
+            this.btnExitRB.UseVisualStyleBackColor = false;
+            // 
+            // btnRefreshRB
+            // 
+            this.btnRefreshRB.BackColor = System.Drawing.Color.Peru;
+            this.btnRefreshRB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRefreshRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefreshRB.Location = new System.Drawing.Point(35, 378);
+            this.btnRefreshRB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRefreshRB.Name = "btnRefreshRB";
+            this.btnRefreshRB.Size = new System.Drawing.Size(113, 37);
+            this.btnRefreshRB.TabIndex = 19;
+            this.btnRefreshRB.Text = "Refresh";
+            this.btnRefreshRB.UseVisualStyleBackColor = false;
+            // 
+            // pbxStuIs
+            // 
+            this.pbxStuIs.Image = ((System.Drawing.Image)(resources.GetObject("pbxStuIs.Image")));
+            this.pbxStuIs.Location = new System.Drawing.Point(73, 22);
+            this.pbxStuIs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbxStuIs.Name = "pbxStuIs";
+            this.pbxStuIs.Size = new System.Drawing.Size(180, 188);
+            this.pbxStuIs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxStuIs.TabIndex = 18;
+            this.pbxStuIs.TabStop = false;
+            // 
+            // btnSearchIsB
+            // 
+            this.btnSearchIsB.BackColor = System.Drawing.Color.Peru;
+            this.btnSearchIsB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSearchIsB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchIsB.Location = new System.Drawing.Point(64, 310);
+            this.btnSearchIsB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSearchIsB.Name = "btnSearchIsB";
+            this.btnSearchIsB.Size = new System.Drawing.Size(191, 37);
+            this.btnSearchIsB.TabIndex = 17;
+            this.btnSearchIsB.Text = "Search Student";
+            this.btnSearchIsB.UseVisualStyleBackColor = false;
+            // 
+            // txtEnrollIsB
+            // 
+            this.txtEnrollIsB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnrollIsB.Location = new System.Drawing.Point(35, 271);
+            this.txtEnrollIsB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEnrollIsB.Name = "txtEnrollIsB";
+            this.txtEnrollIsB.Size = new System.Drawing.Size(253, 26);
+            this.txtEnrollIsB.TabIndex = 16;
+            this.txtEnrollIsB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dgvReturnBook
+            // 
+            this.dgvReturnBook.AllowUserToAddRows = false;
+            this.dgvReturnBook.AllowUserToDeleteRows = false;
+            this.dgvReturnBook.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvReturnBook.BackgroundColor = System.Drawing.Color.PapayaWhip;
+            this.dgvReturnBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReturnBook.Location = new System.Drawing.Point(327, 10);
+            this.dgvReturnBook.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvReturnBook.Name = "dgvReturnBook";
+            this.dgvReturnBook.ReadOnly = true;
+            this.dgvReturnBook.RowHeadersWidth = 62;
+            this.dgvReturnBook.RowTemplate.Height = 28;
+            this.dgvReturnBook.Size = new System.Drawing.Size(907, 470);
+            this.dgvReturnBook.TabIndex = 6;
+            // 
+            // lblEnterIsB
+            // 
+            this.lblEnterIsB.AutoSize = true;
+            this.lblEnterIsB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterIsB.Location = new System.Drawing.Point(31, 230);
+            this.lblEnterIsB.Name = "lblEnterIsB";
+            this.lblEnterIsB.Size = new System.Drawing.Size(252, 25);
+            this.lblEnterIsB.TabIndex = 0;
+            this.lblEnterIsB.Text = "Enter Enrollment Number";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnExitRB);
+            this.panel2.Controls.Add(this.btnRefreshRB);
+            this.panel2.Controls.Add(this.pbxStuIs);
+            this.panel2.Controls.Add(this.btnSearchIsB);
+            this.panel2.Controls.Add(this.txtEnrollIsB);
+            this.panel2.Controls.Add(this.lblEnterIsB);
+            this.panel2.Location = new System.Drawing.Point(11, 10);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(337, 454);
+            this.panel2.TabIndex = 5;
+            // 
+            // ReturnBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(1244, 676);
+            this.Controls.Add(this.pnlInfoRB);
+            this.Controls.Add(this.dgvReturnBook);
+            this.Controls.Add(this.panel2);
+            this.Name = "ReturnBook";
+            this.Text = "ReturnBook";
+            this.pnlInfoRB.ResumeLayout(false);
+            this.pnlInfoRB.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxStuIs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReturnBook)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelRB;
+        private System.Windows.Forms.Button btnReturnRB;
+        private System.Windows.Forms.DateTimePicker dtpBookReturnDateRB;
+        private System.Windows.Forms.TextBox txtBookIssueDateRB;
+        private System.Windows.Forms.TextBox txtBookNameRB;
+        private System.Windows.Forms.Label lblBookReturnDateRB;
+        private System.Windows.Forms.Label lblBookIssueDateRB;
+        private System.Windows.Forms.Label lblBookNameRB;
+        private System.Windows.Forms.Panel pnlInfoRB;
+        private System.Windows.Forms.Button btnExitRB;
+        private System.Windows.Forms.Button btnRefreshRB;
+        private System.Windows.Forms.PictureBox pbxStuIs;
+        private System.Windows.Forms.Button btnSearchIsB;
+        private System.Windows.Forms.TextBox txtEnrollIsB;
+        private System.Windows.Forms.DataGridView dgvReturnBook;
+        private System.Windows.Forms.Label lblEnterIsB;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ Try/ReturnBook.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LIBRARY_MANAGEMENT_SYSTEM
+{
+    public partial class ReturnBook : Form
+    {
+        public ReturnBook()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchIsB_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=LAPTOP-P9NRKPUV\SQLEXPRESS;Initial Catalog=Library_Management_System;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = "select * from IRBook where Stu_enroll = '" + txtEnrollIsB.Text + "'and Book_return_date IS NULL";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                dgvReturnBook.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID or no Book Issued", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        String bdate;
+        String bname;
+        Int64 rowid;
+        private void ReturnBook_Load(object sender, EventArgs e)
+        {
+            pnlInfoRB.Visible = false;
+            this.txtEnrollIsB.Clear();
+        }
+
+        private void dgvReturnBook_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            pnlInfoRB.Visible = true;
+
+            if (dgvReturnBook.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                rowid = Int64.Parse(dgvReturnBook.Rows[e.RowIndex].Cells[0].Value.ToString());
+                bname = dgvReturnBook.Rows[e.RowIndex].Cells[6].Value.ToString();
+                bdate = dgvReturnBook.Rows[e.RowIndex].Cells[7].Value.ToString();
+            }
+            txtBookNameRB.Text = bname;
+            txtBookIssueDateRB.Text = bdate;
+        }
+
+        private void btnReturnRB_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=LAPTOP-P9NRKPUV\SQLEXPRESS;Initial Catalog=Library_Management_System;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "update IRBook set Book_return_date = '" + dtpBookReturnDateRB.Text + "' where Stu_enroll = '" + txtEnrollIsB.Text + "' and BID = " + rowid + "";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Return Succesfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ReturnBook_Load(this, null);
+        }
+
+        private void txtEnrollIsB_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEnrollIsB.Text == "")
+            {
+                pnlInfoRB.Visible = false;
+                dgvReturnBook.DataSource = null;
+            }
+        }
+
+        private void btnRefreshRB_Click(object sender, EventArgs e)
+        {
+            this.txtEnrollIsB.Clear();
+        }
+
+        private void btnExitRB_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCancelRB_Click(object sender, EventArgs e)
+        {
+            pnlInfoRB.Visible = false;
+        }
+    }
+}
