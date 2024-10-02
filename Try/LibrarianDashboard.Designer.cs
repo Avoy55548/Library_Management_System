eafedf0977b5478bc5049ecd33d957e7836562ea
22-48810-3@student.aiub.edu <htsadia2105@gmail.com>
2024-10-02 16:17:33 +0600

    LibrarianDashboard

---
+++ Try/LibrarianDashboard.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Try
+{
+    partial class LibrarianDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LibrarianDashboard));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.BooksToolStripMenuIte = new System.Windows.Forms.ToolStripMenuItem();
+            this.addBooksToolStripMenuIte = new System.Windows.Forms.ToolStripMenuItem();
+            this.ViewBooksToolStripMenuIte = new System.Windows.Forms.ToolStripMenuItem();
+            this.CustomerStripMenuItem21 = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddStudenttoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ViewStudenttoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.issueBooksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.returnBooksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.issueReturnBookInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.LogouttoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Maroon;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BooksToolStripMenuIte,
+            this.CustomerStripMenuItem21,
+            this.toolStripMenuItem1,
+            this.LogouttoolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(1271, 58);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // BooksToolStripMenuIte
+            // 
+            this.BooksToolStripMenuIte.AutoSize = false;
+            this.BooksToolStripMenuIte.BackColor = System.Drawing.Color.Peru;
+            this.BooksToolStripMenuIte.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addBooksToolStripMenuIte,
+            this.ViewBooksToolStripMenuIte});
+            this.BooksToolStripMenuIte.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BooksToolStripMenuIte.Image = ((System.Drawing.Image)(resources.GetObject("BooksToolStripMenuIte.Image")));
+            this.BooksToolStripMenuIte.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.BooksToolStripMenuIte.Name = "BooksToolStripMenuIte";
+            this.BooksToolStripMenuIte.Size = new System.Drawing.Size(237, 56);
+            this.BooksToolStripMenuIte.Text = "Books";
+            // 
+            // addBooksToolStripMenuIte
+            // 
+            this.addBooksToolStripMenuIte.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.addBooksToolStripMenuIte.Name = "addBooksToolStripMenuIte";
+            this.addBooksToolStripMenuIte.Size = new System.Drawing.Size(281, 56);
+            this.addBooksToolStripMenuIte.Text = "Add Books";
+            // 
+            // ViewBooksToolStripMenuIte
+            // 
+            this.ViewBooksToolStripMenuIte.Image = ((System.Drawing.Image)(resources.GetObject("ViewBooksToolStripMenuIte.Image")));
+            this.ViewBooksToolStripMenuIte.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.ViewBooksToolStripMenuIte.Name = "ViewBooksToolStripMenuIte";
+            this.ViewBooksToolStripMenuIte.Size = new System.Drawing.Size(281, 56);
+            this.ViewBooksToolStripMenuIte.Text = "View Books Info";
+            // 
+            // CustomerStripMenuItem21
+            // 
+            this.CustomerStripMenuItem21.AutoSize = false;
+            this.CustomerStripMenuItem21.BackColor = System.Drawing.Color.Peru;
+            this.CustomerStripMenuItem21.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddStudenttoolStripMenuItem,
+            this.ViewStudenttoolStripMenuItem,
+            this.issueBooksToolStripMenuItem,
+            this.returnBooksToolStripMenuItem,
+            this.issueReturnBookInfoToolStripMenuItem});
+            this.CustomerStripMenuItem21.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.CustomerStripMenuItem21.Image = ((System.Drawing.Image)(resources.GetObject("CustomerStripMenuItem21.Image")));
+            this.CustomerStripMenuItem21.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.CustomerStripMenuItem21.Name = "CustomerStripMenuItem21";
+            this.CustomerStripMenuItem21.Size = new System.Drawing.Size(237, 56);
+            this.CustomerStripMenuItem21.Text = "Student";
+            // 
+            // AddStudenttoolStripMenuItem
+            // 
+            this.AddStudenttoolStripMenuItem.AutoSize = false;
+            this.AddStudenttoolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("AddStudenttoolStripMenuItem.Image")));
+            this.AddStudenttoolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.AddStudenttoolStripMenuItem.Name = "AddStudenttoolStripMenuItem";
+            this.AddStudenttoolStripMenuItem.Size = new System.Drawing.Size(296, 60);
+            this.AddStudenttoolStripMenuItem.Text = "Add Student Info";
+            // 
+            // ViewStudenttoolStripMenuItem
+            // 
+            this.ViewStudenttoolStripMenuItem.AutoSize = false;
+            this.ViewStudenttoolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ViewStudenttoolStripMenuItem.Image")));
+            this.ViewStudenttoolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.ViewStudenttoolStripMenuItem.Name = "ViewStudenttoolStripMenuItem";
+            this.ViewStudenttoolStripMenuItem.Size = new System.Drawing.Size(296, 60);
+            this.ViewStudenttoolStripMenuItem.Text = "View Student Info";
+            // 
+            // issueBooksToolStripMenuItem
+            // 
+            this.issueBooksToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("issueBooksToolStripMenuItem.Image")));
+            this.issueBooksToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.issueBooksToolStripMenuItem.Name = "issueBooksToolStripMenuItem";
+            this.issueBooksToolStripMenuItem.Size = new System.Drawing.Size(337, 56);
+            this.issueBooksToolStripMenuItem.Text = "Issue Books";
+            // 
+            // returnBooksToolStripMenuItem
+            // 
+            this.returnBooksToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("returnBooksToolStripMenuItem.Image")));
+            this.returnBooksToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.returnBooksToolStripMenuItem.Name = "returnBooksToolStripMenuItem";
+            this.returnBooksToolStripMenuItem.Size = new System.Drawing.Size(337, 56);
+            this.returnBooksToolStripMenuItem.Text = "Return Books";
+            // 
+            // issueReturnBookInfoToolStripMenuItem
+            // 
+            this.issueReturnBookInfoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("issueReturnBookInfoToolStripMenuItem.Image")));
+            this.issueReturnBookInfoToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.issueReturnBookInfoToolStripMenuItem.Name = "issueReturnBookInfoToolStripMenuItem";
+            this.issueReturnBookInfoToolStripMenuItem.Size = new System.Drawing.Size(337, 56);
+            this.issueReturnBookInfoToolStripMenuItem.Text = "IssueReturn Book Info";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AutoSize = false;
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.Peru;
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.toolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripMenuItem1.Image")));
+            this.toolStripMenuItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(237, 56);
+            this.toolStripMenuItem1.Text = "Accounts";
+            // 
+            // LogouttoolStripMenuItem
+            // 
+            this.LogouttoolStripMenuItem.AutoSize = false;
+            this.LogouttoolStripMenuItem.BackColor = System.Drawing.Color.Peru;
+            this.LogouttoolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.LogouttoolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("LogouttoolStripMenuItem.Image")));
+            this.LogouttoolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.LogouttoolStripMenuItem.Name = "LogouttoolStripMenuItem";
+            this.LogouttoolStripMenuItem.Size = new System.Drawing.Size(237, 56);
+            this.LogouttoolStripMenuItem.Text = "Log out";
+            // 
+            // LibrarianDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1271, 553);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "LibrarianDashboard";
+            this.Text = "LibrarianDashboard";
+            this.Load += new System.EventHandler(this.LibrarianDashboard_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem BooksToolStripMenuIte;
+        private System.Windows.Forms.ToolStripMenuItem addBooksToolStripMenuIte;
+        private System.Windows.Forms.ToolStripMenuItem ViewBooksToolStripMenuIte;
+        private System.Windows.Forms.ToolStripMenuItem CustomerStripMenuItem21;
+        private System.Windows.Forms.ToolStripMenuItem AddStudenttoolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ViewStudenttoolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem issueBooksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem returnBooksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem issueReturnBookInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem LogouttoolStripMenuItem;
+    }
+}
---
+++ Try/LibrarianDashboard.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LIBRARY_MANAGEMENT_SYSTEM
+{
+    public partial class LibrarianDashboard : Form
+    {
+        public LibrarianDashboard()
+        {
+            InitializeComponent();
+        }
+
+
+        private void addBooksToolStripMenuIte_Click_1(object sender, EventArgs e)
+        {
+            AddBooks ab = new AddBooks();
+            ab.Show();
+        }
+
+        private void ViewBooksToolStripMenuIte_Click(object sender, EventArgs e)
+        {
+            ViewBookInfo vbi = new ViewBookInfo();
+            vbi.Show();
+        }
+
+        private void LogouttoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to log out??", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                this.Close();
+                Login lg = new Login();
+                lg.Show();
+            }
+        }
+
+        private void issueBooksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IssueBooks ib = new IssueBooks();
+            ib.Show();
+        }
+
+        private void returnBooksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReturnBook rb = new ReturnBook();
+            rb.Show();
+        }
+
+        private void ViewStudenttoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ViewStudentInfo vsi = new ViewStudentInfo();
+            vsi.Show();
+        }
+
+        private void AddStudenttoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddStudentInfo Stu = new AddStudentInfo();
+            Stu.Show();
+        }
+
+        private void issueReturnBookInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IssueReturnBookInfo irbi = new IssueReturnBookInfo();
+            irbi.Show();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Accounts ac = new Accounts();
+            ac.Show();
+        }
+
+        private void LibrarianDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
