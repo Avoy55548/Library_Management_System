e721f293292395f857f9a9f2f9f860e73330ae7d
22-48810-3@student.aiub.edu <htsadia2105@gmail.com>
2024-10-02 16:06:16 +0600

    Add Books

---
+++ Try/AddBooks.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace LIBRARY_MANAGEMENT_SYSTEM
+{
+    partial class AddBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddBooks));
+            this.pbxPicAB = new System.Windows.Forms.PictureBox();
+            this.txtBookPriceAB = new System.Windows.Forms.TextBox();
+            this.lblBookPriceVBI = new System.Windows.Forms.Label();
+            this.btnCancelAB = new System.Windows.Forms.Button();
+            this.btnSaveAB = new System.Windows.Forms.Button();
+            this.txtBookQuantityAB = new System.Windows.Forms.TextBox();
+            this.lblBookQuantityAB = new System.Windows.Forms.Label();
+            this.txtISBNNoAB = new System.Windows.Forms.TextBox();
+            this.txtBookPubAB = new System.Windows.Forms.TextBox();
+            this.lblTitleAB = new System.Windows.Forms.Label();
+            this.pnlTitleAb = new System.Windows.Forms.Panel();
+            this.pbxTitlePicAB = new System.Windows.Forms.PictureBox();
+            this.txtBookAuthorAB = new System.Windows.Forms.TextBox();
+            this.txtBookNameAB = new System.Windows.Forms.TextBox();
+            this.lblBookPubAB = new System.Windows.Forms.Label();
+            this.lblBookAuthorAB = new System.Windows.Forms.Label();
+            this.lblBookNameAB = new System.Windows.Forms.Label();
+            this.lblISBNNoAB = new System.Windows.Forms.Label();
+            this.pnlInfoAB = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPicAB)).BeginInit();
+            this.pnlTitleAb.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTitlePicAB)).BeginInit();
+            this.pnlInfoAB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbxPicAB
+            // 
+            this.pbxPicAB.Image = ((System.Drawing.Image)(resources.GetObject("pbxPicAB.Image")));
+            this.pbxPicAB.Location = new System.Drawing.Point(0, 65);
+            this.pbxPicAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbxPicAB.Name = "pbxPicAB";
+            this.pbxPicAB.Size = new System.Drawing.Size(369, 526);
+            this.pbxPicAB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPicAB.TabIndex = 20;
+            this.pbxPicAB.TabStop = false;
+            // 
+            // txtBookPriceAB
+            // 
+            this.txtBookPriceAB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBookPriceAB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookPriceAB.Location = new System.Drawing.Point(241, 290);
+            this.txtBookPriceAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookPriceAB.Name = "txtBookPriceAB";
+            this.txtBookPriceAB.Size = new System.Drawing.Size(293, 26);
+            this.txtBookPriceAB.TabIndex = 42;
+            this.txtBookPriceAB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBookPriceVBI
+            // 
+            this.lblBookPriceVBI.AutoSize = true;
+            this.lblBookPriceVBI.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookPriceVBI.Location = new System.Drawing.Point(59, 300);
+            this.lblBookPriceVBI.Name = "lblBookPriceVBI";
+            this.lblBookPriceVBI.Size = new System.Drawing.Size(105, 23);
+            this.lblBookPriceVBI.TabIndex = 41;
+            this.lblBookPriceVBI.Text = "Book Price";
+            // 
+            // btnCancelAB
+            // 
+            this.btnCancelAB.BackColor = System.Drawing.Color.IndianRed;
+            this.btnCancelAB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancelAB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelAB.Location = new System.Drawing.Point(395, 426);
+            this.btnCancelAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancelAB.Name = "btnCancelAB";
+            this.btnCancelAB.Size = new System.Drawing.Size(117, 41);
+            this.btnCancelAB.TabIndex = 21;
+            this.btnCancelAB.Text = "Cancel";
+            this.btnCancelAB.UseVisualStyleBackColor = false;
+            // 
+            // btnSaveAB
+            // 
+            this.btnSaveAB.BackColor = System.Drawing.Color.IndianRed;
+            this.btnSaveAB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSaveAB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAB.Location = new System.Drawing.Point(209, 426);
+            this.btnSaveAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSaveAB.Name = "btnSaveAB";
+            this.btnSaveAB.Size = new System.Drawing.Size(125, 41);
+            this.btnSaveAB.TabIndex = 20;
+            this.btnSaveAB.Text = "Save";
+            this.btnSaveAB.UseVisualStyleBackColor = false;
+            // 
+            // txtBookQuantityAB
+            // 
+            this.txtBookQuantityAB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBookQuantityAB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookQuantityAB.Location = new System.Drawing.Point(241, 340);
+            this.txtBookQuantityAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookQuantityAB.Name = "txtBookQuantityAB";
+            this.txtBookQuantityAB.Size = new System.Drawing.Size(293, 26);
+            this.txtBookQuantityAB.TabIndex = 19;
+            this.txtBookQuantityAB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBookQuantityAB
+            // 
+            this.lblBookQuantityAB.AutoSize = true;
+            this.lblBookQuantityAB.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookQuantityAB.Location = new System.Drawing.Point(59, 343);
+            this.lblBookQuantityAB.Name = "lblBookQuantityAB";
+            this.lblBookQuantityAB.Size = new System.Drawing.Size(141, 23);
+            this.lblBookQuantityAB.TabIndex = 18;
+            this.lblBookQuantityAB.Text = "Book Quantity";
+            // 
+            // txtISBNNoAB
+            // 
+            this.txtISBNNoAB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtISBNNoAB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtISBNNoAB.Location = new System.Drawing.Point(241, 244);
+            this.txtISBNNoAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtISBNNoAB.Name = "txtISBNNoAB";
+            this.txtISBNNoAB.Size = new System.Drawing.Size(293, 26);
+            this.txtISBNNoAB.TabIndex = 10;
+            this.txtISBNNoAB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBookPubAB
+            // 
+            this.txtBookPubAB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBookPubAB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookPubAB.Location = new System.Drawing.Point(241, 194);
+            this.txtBookPubAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookPubAB.Name = "txtBookPubAB";
+            this.txtBookPubAB.Size = new System.Drawing.Size(293, 26);
+            this.txtBookPubAB.TabIndex = 9;
+            this.txtBookPubAB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTitleAB
+            // 
+            this.lblTitleAB.AutoSize = true;
+            this.lblTitleAB.Font = new System.Drawing.Font("Book Antiqua", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleAB.ForeColor = System.Drawing.Color.Black;
+            this.lblTitleAB.Location = new System.Drawing.Point(405, 11);
+            this.lblTitleAB.Name = "lblTitleAB";
+            this.lblTitleAB.Size = new System.Drawing.Size(135, 28);
+            this.lblTitleAB.TabIndex = 0;
+            this.lblTitleAB.Text = "Add Books";
+            // 
+            // pnlTitleAb
+            // 
+            this.pnlTitleAb.BackColor = System.Drawing.Color.RosyBrown;
+            this.pnlTitleAb.Controls.Add(this.lblTitleAB);
+            this.pnlTitleAb.Controls.Add(this.pbxTitlePicAB);
+            this.pnlTitleAb.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitleAb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlTitleAb.Name = "pnlTitleAb";
+            this.pnlTitleAb.Size = new System.Drawing.Size(872, 71);
+            this.pnlTitleAb.TabIndex = 18;
+            // 
+            // pbxTitlePicAB
+            // 
+            this.pbxTitlePicAB.Image = ((System.Drawing.Image)(resources.GetObject("pbxTitlePicAB.Image")));
+            this.pbxTitlePicAB.Location = new System.Drawing.Point(312, -6);
+            this.pbxTitlePicAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbxTitlePicAB.Name = "pbxTitlePicAB";
+            this.pbxTitlePicAB.Size = new System.Drawing.Size(89, 66);
+            this.pbxTitlePicAB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxTitlePicAB.TabIndex = 1;
+            this.pbxTitlePicAB.TabStop = false;
+            // 
+            // txtBookAuthorAB
+            // 
+            this.txtBookAuthorAB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBookAuthorAB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookAuthorAB.Location = new System.Drawing.Point(241, 135);
+            this.txtBookAuthorAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookAuthorAB.Name = "txtBookAuthorAB";
+            this.txtBookAuthorAB.Size = new System.Drawing.Size(293, 26);
+            this.txtBookAuthorAB.TabIndex = 8;
+            this.txtBookAuthorAB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBookNameAB
+            // 
+            this.txtBookNameAB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBookNameAB.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBookNameAB.Location = new System.Drawing.Point(241, 79);
+            this.txtBookNameAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBookNameAB.Name = "txtBookNameAB";
+            this.txtBookNameAB.Size = new System.Drawing.Size(293, 26);
+            this.txtBookNameAB.TabIndex = 7;
+            this.txtBookNameAB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBookPubAB
+            // 
+            this.lblBookPubAB.AutoSize = true;
+            this.lblBookPubAB.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookPubAB.Location = new System.Drawing.Point(59, 194);
+            this.lblBookPubAB.Name = "lblBookPubAB";
+            this.lblBookPubAB.Size = new System.Drawing.Size(163, 23);
+            this.lblBookPubAB.TabIndex = 3;
+            this.lblBookPubAB.Text = "Book Publication";
+            // 
+            // lblBookAuthorAB
+            // 
+            this.lblBookAuthorAB.AutoSize = true;
+            this.lblBookAuthorAB.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookAuthorAB.Location = new System.Drawing.Point(59, 142);
+            this.lblBookAuthorAB.Name = "lblBookAuthorAB";
+            this.lblBookAuthorAB.Size = new System.Drawing.Size(125, 23);
+            this.lblBookAuthorAB.TabIndex = 2;
+            this.lblBookAuthorAB.Text = "Book Author";
+            // 
+            // lblBookNameAB
+            // 
+            this.lblBookNameAB.AutoSize = true;
+            this.lblBookNameAB.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBookNameAB.Location = new System.Drawing.Point(59, 85);
+            this.lblBookNameAB.Name = "lblBookNameAB";
+            this.lblBookNameAB.Size = new System.Drawing.Size(115, 23);
+            this.lblBookNameAB.TabIndex = 1;
+            this.lblBookNameAB.Text = "Book Name";
+            // 
+            // lblISBNNoAB
+            // 
+            this.lblISBNNoAB.AutoSize = true;
+            this.lblISBNNoAB.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblISBNNoAB.Location = new System.Drawing.Point(59, 254);
+            this.lblISBNNoAB.Name = "lblISBNNoAB";
+            this.lblISBNNoAB.Size = new System.Drawing.Size(95, 23);
+            this.lblISBNNoAB.TabIndex = 6;
+            this.lblISBNNoAB.Text = "ISBN No.";
+            // 
+            // pnlInfoAB
+            // 
+            this.pnlInfoAB.BackColor = System.Drawing.Color.RosyBrown;
+            this.pnlInfoAB.Controls.Add(this.txtBookPriceAB);
+            this.pnlInfoAB.Controls.Add(this.lblBookPriceVBI);
+            this.pnlInfoAB.Controls.Add(this.btnCancelAB);
+            this.pnlInfoAB.Controls.Add(this.btnSaveAB);
+            this.pnlInfoAB.Controls.Add(this.txtBookQuantityAB);
+            this.pnlInfoAB.Controls.Add(this.lblBookQuantityAB);
+            this.pnlInfoAB.Controls.Add(this.txtISBNNoAB);
+            this.pnlInfoAB.Controls.Add(this.txtBookPubAB);
+            this.pnlInfoAB.Controls.Add(this.txtBookAuthorAB);
+            this.pnlInfoAB.Controls.Add(this.txtBookNameAB);
+            this.pnlInfoAB.Controls.Add(this.lblISBNNoAB);
+            this.pnlInfoAB.Controls.Add(this.lblBookPubAB);
+            this.pnlInfoAB.Controls.Add(this.lblBookAuthorAB);
+            this.pnlInfoAB.Controls.Add(this.lblBookNameAB);
+            this.pnlInfoAB.Location = new System.Drawing.Point(316, 65);
+            this.pnlInfoAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlInfoAB.Name = "pnlInfoAB";
+            this.pnlInfoAB.Size = new System.Drawing.Size(556, 526);
+            this.pnlInfoAB.TabIndex = 19;
+            // 
+            // AddBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 591);
+            this.Controls.Add(this.pbxPicAB);
+            this.Controls.Add(this.pnlTitleAb);
+            this.Controls.Add(this.pnlInfoAB);
+            this.Name = "AddBooks";
+            this.Text = "AddBooks";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPicAB)).EndInit();
+            this.pnlTitleAb.ResumeLayout(false);
+            this.pnlTitleAb.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTitlePicAB)).EndInit();
+            this.pnlInfoAB.ResumeLayout(false);
+            this.pnlInfoAB.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxPicAB;
+        private System.Windows.Forms.TextBox txtBookPriceAB;
+        private System.Windows.Forms.Label lblBookPriceVBI;
+        private System.Windows.Forms.Button btnCancelAB;
+        private System.Windows.Forms.Button btnSaveAB;
+        private System.Windows.Forms.TextBox txtBookQuantityAB;
+        private System.Windows.Forms.Label lblBookQuantityAB;
+        private System.Windows.Forms.TextBox txtISBNNoAB;
+        private System.Windows.Forms.TextBox txtBookPubAB;
+        private System.Windows.Forms.Label lblTitleAB;
+        private System.Windows.Forms.Panel pnlTitleAb;
+        private System.Windows.Forms.PictureBox pbxTitlePicAB;
+        private System.Windows.Forms.TextBox txtBookAuthorAB;
+        private System.Windows.Forms.TextBox txtBookNameAB;
+        private System.Windows.Forms.Label lblBookPubAB;
+        private System.Windows.Forms.Label lblBookAuthorAB;
+        private System.Windows.Forms.Label lblBookNameAB;
+        private System.Windows.Forms.Label lblISBNNoAB;
+        private System.Windows.Forms.Panel pnlInfoAB;
+    }
+}
---
+++ Try/AddBooks.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LIBRARY_MANAGEMENT_SYSTEM
+{
+    public partial class AddBooks : Form
+    {
+        //private DataAccess Da { get; set; }
+        public AddBooks()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValidToSave()
+        {
+            if (String.IsNullOrEmpty(this.txtBookPubAB.Text) || String.IsNullOrEmpty(this.txtBookAuthorAB.Text) ||
+                String.IsNullOrEmpty(this.txtBookNameAB.Text) || String.IsNullOrEmpty(this.txtISBNNoAB.Text) ||
+                String.IsNullOrEmpty(this.txtBookQuantityAB.Text) || String.IsNullOrEmpty(this.txtBookPriceAB.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void ClearAll()
+        {
+            this.txtBookPubAB.Clear();
+            this.txtBookAuthorAB.Clear();
+            this.txtBookNameAB.Clear();
+            this.txtISBNNoAB.Clear();
+            this.txtBookQuantityAB.Clear();
+            this.txtBookPriceAB.Clear();
+        }
+
+        private void btnSaveAB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!IsValidToSave())
+                {
+                    MessageBox.Show("Please fill all the information");
+                    return;
+                }
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = @"Data Source=LAPTOP-P9NRKPUV\SQLEXPRESS;Initial Catalog=Library_Management_System;Integrated Security=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                con.Open();
+                cmd.CommandText = "insert into Book (Name,Author,Publication,ISBN,Quantity,Price) values ('" + this.txtBookNameAB.Text + "','" + this.txtBookAuthorAB.Text + "','" + this.txtBookPubAB.Text + "', '" + this.txtISBNNoAB.Text + "'," + this.txtBookQuantityAB.Text + ", " + this.txtBookPriceAB.Text + " )";
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Data Saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.ClearAll();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("There is an error in your input: " + exc.Message);
+            }
+        }
+
+        private void btnCancelAB_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will Remove you all unsaved data", "Are you sure to perform this??", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+
+    }
+}
