f1681d79abfb0f0ce089831f3b8121a8b0fc32dc
Sayed Saifur Rahman <saifurrahmans306@gmail.com>
2024-10-05 02:11:03 +0600

    updated

--- Try/ViewBookInfo.cs
+++ Try/ViewBookInfo.cs
@@ -32,35 +32,21 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
                 btnRemoveVBI.Visible = true;
             }
 
-
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = @"Data Source=DESKTOP-8PBDEDF\SQLEXPRESS;Initial Catalog=saif;Integrated Security=True;Encrypt=False";
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = con;
-
-            cmd.CommandText = "SELECT * FROM Book";
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataSet ds = new DataSet();
-            da.Fill(ds);
-
-            dgvViewBooks.DataSource = ds.Tables[0];
+            RefreshDataGridView(); // Load data into the grid view on form load
         }
 
         int BookID;
         Int64 rowid;
 
         private void dgvViewBooks_CellClick(object sender, DataGridViewCellEventArgs e)
-
         {
             try
-
             {
                 if (dgvViewBooks.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
-
                 {
                     BookID = int.Parse(dgvViewBooks.Rows[e.RowIndex].Cells[0].Value.ToString());
-                   
                 }
+
                 pnlURViewBooks.Visible = true;
                 SqlConnection con = new SqlConnection();
                 con.ConnectionString = @"Data Source=DESKTOP-8PBDEDF\SQLEXPRESS;Initial Catalog=saif;Integrated Security=True;Encrypt=False";
@@ -73,6 +59,7 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
                 da.Fill(ds);
 
                 rowid = Int64.Parse(ds.Tables[0].Rows[0][0].ToString());
+
                 txtBookNameVB.Text = ds.Tables[0].Rows[0][1].ToString();
                 txtBookAuthorVB.Text = ds.Tables[0].Rows[0][2].ToString();
                 txtBookPublicationVB.Text = ds.Tables[0].Rows[0][3].ToString();
@@ -85,11 +72,9 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
             {
                 MessageBox.Show("There is an error in your input: " + exc.Message);
             }
-
         }
 
         private void btnCancelVBI_Click(object sender, EventArgs e)
-
         {
             pnlURViewBooks.Visible = false;
         }
@@ -112,61 +97,94 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
             }
             else
             {
-                SqlConnection con = new SqlConnection();
-                con.ConnectionString = @"Data Source=DESKTOP-8PBDEDF\SQLEXPRESS;Initial Catalog=saif;Integrated Security=True;Encrypt=False";
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = con;
-
-                cmd.CommandText = "SELECT * FROM Book";
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
-
-                dgvViewBooks.DataSource = ds.Tables[0];
+                RefreshDataGridView(); // Refresh data when the search box is cleared
             }
         }
 
-
         private void btnUpdateViewBooks_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Data will be Updated. Confirm?", "Success", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            if (MessageBox.Show("Data will be updated. Confirm?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
             {
                 SqlConnection con = new SqlConnection();
                 con.ConnectionString = @"Data Source=DESKTOP-8PBDEDF\SQLEXPRESS;Initial Catalog=saif;Integrated Security=True;Encrypt=False";
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = con;
 
-                cmd.CommandText = "update Book set Name = '" + this.txtBookNameVB.Text + "', Author = '" + this.txtBookAuthorVB.Text + "',Publication = '" + this.txtBookPublicationVB.Text + "', ISBN = '" + this.txtISBNNoVB.Text + "',Quantity = " + this.txtBookQuantityVB.Text + ", Price = " + this.txtBookPriceVB.Text + " where BookID = " + rowid + " ";
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
+                try
+                {
+                    con.Open();
+                    // Update the book details
+                    cmd.CommandText = "UPDATE Book SET Name = '" + this.txtBookNameVB.Text + "', Author = '" + this.txtBookAuthorVB.Text + "', Publication = '" + this.txtBookPublicationVB.Text + "', ISBN = '" + this.txtISBNNoVB.Text + "', Quantity = " + this.txtBookQuantityVB.Text + ", Price = " + this.txtBookPriceVB.Text + " WHERE BookID = " + rowid;
+                    cmd.ExecuteNonQuery();
+
+                    // Refresh the DataGridView after the update
+                    RefreshDataGridView();
+
+                    MessageBox.Show("Book details updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error while updating book details: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    con.Close();
+                }
             }
         }
 
         private void btnRemoveVBI_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Data will be Updated. Confirm?", "Confirmation Dialog", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            if (MessageBox.Show("Data will be deleted. Confirm?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
             {
                 SqlConnection con = new SqlConnection();
                 con.ConnectionString = @"Data Source=DESKTOP-8PBDEDF\SQLEXPRESS;Initial Catalog=saif;Integrated Security=True;Encrypt=False";
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = con;
 
-                cmd.CommandText = "delete from Book where BookID = " + rowid + "";
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
+                try
+                {
+                    con.Open();
+                    cmd.CommandText = "DELETE FROM Book WHERE BookID = " + rowid;
+                    cmd.ExecuteNonQuery();
+
+                    // Refresh the DataGridView after the delete
+                    RefreshDataGridView();
+
+                    MessageBox.Show("Book removed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error while removing book: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    con.Close();
+                }
             }
         }
 
         private void btnRefreshViewBook_Click(object sender, EventArgs e)
-
         {
             txtSearchViewBook.Clear();
             pnlURViewBooks.Visible = false;
+            RefreshDataGridView(); // Ensure grid is refreshed after clearing the search
         }
 
+        private void RefreshDataGridView()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=DESKTOP-8PBDEDF\SQLEXPRESS;Initial Catalog=saif;Integrated Security=True;Encrypt=False";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
 
+            cmd.CommandText = "SELECT * FROM Book";
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dgvViewBooks.DataSource = ds.Tables[0];
+        }
 
         private void pnlURViewBooks_Paint(object sender, PaintEventArgs e)
         {
