37921d659d0058db957ac999bc96d5e078790c73
USER <bakhtiarsara007@gmail.com>
2024-10-05 15:07:09 +0600

    done2

--- Try/AddLibrarian.Designer.cs
+++ Try/AddLibrarian.Designer.cs
@@ -29,24 +29,13 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.RosyBrown;
-            this.panel2.Location = new System.Drawing.Point(0, -28);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(927, 80);
-            this.panel2.TabIndex = 22;
             // 
             // AddLibrarian
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(929, 684);
-            this.Controls.Add(this.panel2);
             this.Name = "AddLibrarian";
             this.Text = "AddLibrarian";
             this.ResumeLayout(false);
@@ -54,6 +43,5 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel2;
     }
 }
