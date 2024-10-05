413cc37cec2b302032535fc5fb724a943a8da8dc
USER <bakhtiarsara007@gmail.com>
2024-10-05 15:08:21 +0600

    added design

--- Try/AddLibrarian.Designer.cs
+++ Try/AddLibrarian.Designer.cs
@@ -38,6 +38,12 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblTitleAL = new System.Windows.Forms.Label();
             this.pnlInfoAB = new System.Windows.Forms.Panel();
+            this.txtUserIDAL = new System.Windows.Forms.TextBox();
+            this.txtPasswordAL = new System.Windows.Forms.TextBox();
+            this.txtEnrollAL = new System.Windows.Forms.TextBox();
+            this.lblUserIDAL = new System.Windows.Forms.Label();
+            this.lblPasswordAL = new System.Windows.Forms.Label();
+            this.lblENrollAL = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbxPicAB)).BeginInit();
             this.panel2.SuspendLayout();
             this.pnlInfoAB.SuspendLayout();
@@ -129,15 +135,90 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
             // pnlInfoAB
             // 
             this.pnlInfoAB.BackColor = System.Drawing.Color.RosyBrown;
+            this.pnlInfoAB.Controls.Add(this.lblENrollAL);
+            this.pnlInfoAB.Controls.Add(this.txtEnrollAL);
             this.pnlInfoAB.Controls.Add(this.btnClearAL);
             this.pnlInfoAB.Controls.Add(this.btnPrintAL);
             this.pnlInfoAB.Controls.Add(this.btnCancelAL);
             this.pnlInfoAB.Controls.Add(this.btnSaveAL);
+            this.pnlInfoAB.Controls.Add(this.txtPasswordAL);
+            this.pnlInfoAB.Controls.Add(this.txtUserIDAL);
+            this.pnlInfoAB.Controls.Add(this.lblPasswordAL);
+            this.pnlInfoAB.Controls.Add(this.lblUserIDAL);
             this.pnlInfoAB.Location = new System.Drawing.Point(428, 50);
             this.pnlInfoAB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlInfoAB.Name = "pnlInfoAB";
             this.pnlInfoAB.Size = new System.Drawing.Size(500, 662);
             this.pnlInfoAB.TabIndex = 21;
+            // 
+            // txtUserIDAL
+            // 
+            this.txtUserIDAL.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUserIDAL.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserIDAL.Location = new System.Drawing.Point(223, 7);
+            this.txtUserIDAL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtUserIDAL.Name = "txtUserIDAL";
+            this.txtUserIDAL.Size = new System.Drawing.Size(263, 26);
+            this.txtUserIDAL.TabIndex = 7;
+            this.txtUserIDAL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPasswordAL
+            // 
+            this.txtPasswordAL.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPasswordAL.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordAL.Location = new System.Drawing.Point(223, 70);
+            this.txtPasswordAL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPasswordAL.Name = "txtPasswordAL";
+            this.txtPasswordAL.Size = new System.Drawing.Size(263, 26);
+            this.txtPasswordAL.TabIndex = 8;
+            this.txtPasswordAL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtEnrollAL
+            // 
+            this.txtEnrollAL.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEnrollAL.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnrollAL.Location = new System.Drawing.Point(223, 132);
+            this.txtEnrollAL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEnrollAL.Name = "txtEnrollAL";
+            this.txtEnrollAL.Size = new System.Drawing.Size(263, 26);
+            this.txtEnrollAL.TabIndex = 36;
+            this.txtEnrollAL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblUserIDAL
+            // 
+            this.lblUserIDAL.AutoSize = true;
+            this.lblUserIDAL.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserIDAL.Location = new System.Drawing.Point(35, 9);
+            this.lblUserIDAL.Name = "lblUserIDAL";
+            this.lblUserIDAL.Size = new System.Drawing.Size(113, 24);
+            this.lblUserIDAL.TabIndex = 1;
+            this.lblUserIDAL.Text = "User Name";
+            // 
+            // lblPasswordAL
+            // 
+            this.lblPasswordAL.AutoSize = true;
+            this.lblPasswordAL.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordAL.Location = new System.Drawing.Point(35, 71);
+            this.lblPasswordAL.Name = "lblPasswordAL";
+            this.lblPasswordAL.Size = new System.Drawing.Size(98, 24);
+            this.lblPasswordAL.TabIndex = 2;
+            this.lblPasswordAL.Text = "Password";
+            // 
+            // lblENrollAL
+            // 
+            this.lblENrollAL.AutoSize = true;
+            this.lblENrollAL.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblENrollAL.Location = new System.Drawing.Point(41, 132);
+            this.lblENrollAL.Name = "lblENrollAL";
+            this.lblENrollAL.Size = new System.Drawing.Size(67, 24);
+            this.lblENrollAL.TabIndex = 37;
+            this.lblENrollAL.Text = "Enroll";
             // 
             // AddLibrarian
             // 
@@ -153,6 +234,7 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.pnlInfoAB.ResumeLayout(false);
+            this.pnlInfoAB.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -167,5 +249,11 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label lblTitleAL;
         private System.Windows.Forms.Panel pnlInfoAB;
+        private System.Windows.Forms.Label lblENrollAL;
+        private System.Windows.Forms.TextBox txtEnrollAL;
+        private System.Windows.Forms.TextBox txtPasswordAL;
+        private System.Windows.Forms.TextBox txtUserIDAL;
+        private System.Windows.Forms.Label lblPasswordAL;
+        private System.Windows.Forms.Label lblUserIDAL;
     }
 }
