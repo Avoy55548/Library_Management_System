3b650b1c5299fad160695fe91e7bbf76d83d8d95
Tasin Islam Avoy <avoytasin@gmail.com>
2024-10-06 19:10:46 +0600

    addStudent fix

--- Try/AddStudentInfo.cs
+++ Try/AddStudentInfo.cs
@@ -225,19 +225,6 @@ namespace LIBRARY_MANAGEMENT_SYSTEM
             }
         }
 
-        private void dtpDateOfBirthAS_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void AddStudentInfo_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void cmbGenderAS_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
